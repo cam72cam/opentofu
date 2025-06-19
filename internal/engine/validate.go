47cb7c26712287c483c3ea8609c1cadabbf96be2
Christian Mesh <christianmesh1@gmail.com>
2025-06-19 19:27:11 -0400

    Set concurrency to 1
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/engine/validate.go
+++ internal/engine/validate.go
@@ -53,7 +53,7 @@ func WalkValidate(ctx context.Context, config *configs.Config, plugins plugins.M
 
 	root := NewModule(ctx, addrs.RootModuleInstance, config, inputs, scope)
 
-	p := NewConcurrencyPool(10)
+	p := NewConcurrencyPool(1)
 	root.Collect(p)
 	edges, diags := p.Wait()
 	//spew.Dump(edges)
